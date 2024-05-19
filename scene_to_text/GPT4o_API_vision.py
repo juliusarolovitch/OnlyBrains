0e3e4271521900699e9f2884d6f8c2abb8251df5
Robert Devenyi <devenyirobert6@gmail.com>
2024-05-20 01:07:02 +0200

    added scene to text

---
+++ scene_to_text/GPT4o_API_vision.py
@@ -0,0 +1,145 @@
+'''from openai import OpenAI
+# single image
+client = OpenAI(api_key = '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')
+
+response = client.chat.completions.create(
+  model="gpt-4o",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {"type": "text", "text": "What's in this image?"},
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+          },
+        },
+      ],
+    }
+  ],
+  max_tokens=300,
+)
+
+print(response.choices[0])'''
+
+# multiple images
+'''
+client = OpenAI()
+response = client.chat.completions.create(
+  model="gpt-4o",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What are in these images? Is there any difference between them?",
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+          },
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+          },
+        },
+      ],
+    }
+  ],
+  max_tokens=300,
+)
+print(response.choices[0])
+
+#local images
+import base64
+import requests
+
+# OpenAI API Key
+api_key = "YOUR_OPENAI_API_KEY"
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+image_path = "path_to_your_image.jpg"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
+}
+
+payload = {
+  "model": "gpt-4o",
+  "messages": [
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What’s in this image?"
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}"
+          }
+        }
+      ]
+    }
+  ],
+  "max_tokens": 300
+}
+
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json()) '''
+
+# video input
+
+import cv2  # We're using OpenCV to read video, to install !pip install opencv-python
+import base64
+import time
+import os
+import requests
+from openai import OpenAI
+
+client = OpenAI(api_key="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")
+video = cv2.VideoCapture("/Users/deven/SynologyDrive/OnlyBrains/OnlyBrains/scene_to_text/bison.mp4")
+base64Frames = []
+while video.isOpened():
+    success, frame = video.read()
+    if not success:
+        break
+    _, buffer = cv2.imencode(".jpg", frame)
+    base64Frames.append(base64.b64encode(buffer).decode("utf-8"))
+
+video.release()
+print(len(base64Frames), "frames read.")
+
+PROMPT_MESSAGES = [
+    {
+        "role": "user",
+        "content": [
+            "These are frames from a video that I want to upload. Generate a compelling description that I can upload along with the video.",
+            *map(lambda x: {"image": x, "resize": 768}, base64Frames[0::50]),
+        ],
+    },
+]
+params = {
+    "model": "gpt-4o",
+    "messages": PROMPT_MESSAGES,
+    "max_tokens": 200,
+}
+
+result = client.chat.completions.create(**params)
+print(result.choices[0].message.content)
